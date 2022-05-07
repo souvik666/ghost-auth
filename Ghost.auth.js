15191d077386b4a0c0d227c615a9aec94a6e3967
souvik666 <peacedynasty0@gmail.com>
2022-05-07 18:52:28 +0530

    update

--- Ghost.auth.js
+++ Ghost.auth.js
@@ -1,5 +1,5 @@
 import instance from "./src/config/Axios.Config.js";
-//const t = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mjc1MmYzYjljOTMwMmQ0ZTE2MTViZmMiLCJuYW1lIjoic291dmlrIiwiZW1haWwiOiJhZG1pbjExQGdtYWlsLmNvbSIsInRva2VuU2VjcmV0IjoiZGZlNWQwZjRlMjE2MmU5YzYxYmJlM2I4N2Y5NmVkZWU0YWM2NWQ0NjU1ZmMzODkyIiwiX192IjowLCJpYXQiOjE2NTE4NDY5ODAsImV4cCI6MTY4MzM4Mjk4MH0.sxorFaxt-vDqcvqPeHqC3NrzP6fUzrRAt_YZNoZ_j8k`;
+const t = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2Mjc1MmYzYjljOTMwMmQ0ZTE2MTViZmMiLCJuYW1lIjoic291dmlrIiwiZW1haWwiOiJhZG1pbjExQGdtYWlsLmNvbSIsInRva2VuU2VjcmV0IjoiZGZlNWQwZjRlMjE2MmU5YzYxYmJlM2I4N2Y5NmVkZWU0YWM2NWQ0NjU1ZmMzODkyIiwiX192IjowLCJpYXQiOjE2NTE4NDY5ODAsImV4cCI6MTY4MzM4Mjk4MH0.sxorFaxt-vDqcvqPeHqC3NrzP6fUzrRAt_YZNoZ_j8k`;
 
 export default class GhostAuth {
   constructor(ApiKey) {
@@ -45,23 +45,27 @@ export default class GhostAuth {
       .then((d) => {
         return d.data;
       })
-      .catch((d) => {
-        return d;
+      .catch((e) => {
+        return e.data;
       });
   }
 
   async isAuth() {
     return instance
-      .post("/v1/user/api", {
-        headers: {
-          token: this.ApiKey,
-        },
-      })
+      .post(
+        "/v1/user",
+        { token: this.ApiKey },
+        {
+          headers: {
+            token: this.ApiKey,
+          },
+        }
+      )
       .then((d) => {
         return d.data;
       })
-      .catch((d) => {
-        return d.data;
+      .catch((e) => {
+        return e;
       });
   }
 
@@ -75,21 +79,19 @@ export default class GhostAuth {
       .then((d) => {
         return d.data;
       })
-      .catch((d) => {
-        return d;
+      .catch((e) => {
+        return e.data;
       });
   }
 }
 
-const ghost = new GhostAuth(
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluMTExQGdtYWlsLmNvbSIsIm5hbWUiOiJzb3V2aWsiLCJpYXQiOjE2NTE5MjAxOTIsImV4cCI6MTY4MzQ1NjE5Mn0.-vwcaAXsBsAV150wLoRCkPfaKgRA6QZ7Rc7lmywnDLA"
-);
-
+const ghost = new GhostAuth(t);
 /* ghost
   .Login({ email: "admin12@gmail.com", password: "Admin@12345" })
   .then((d) => {
     console.log(d);
   }); */
+
 ghost.isAuth().then((d) => {
   console.log(d);
 });
